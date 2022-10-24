8a8bb9e444418c2fd7fe759626bfaa44e8cf2ef0
Chris41700 <ChrisHui417@gmail.com>
2022-10-23 20:29:36 -0400

    first fit algorithm works - needs testing

--- CSC305 - Lab 3/Processes.h
+++ CSC305 - Lab 3/Processes.h
@@ -34,31 +34,61 @@ void firstFit(vector<int> partitionSize, int m, vector<int> processSize, int n, 
 	for (int i = 0; i < n; i++) {
 		process[i].status = "wait";
 		for (int j = 0; j < m; j++) {
-			partition[j].inUsed = false;
 			if (partitionSize[j] >= processSize[i] && partition[j].inUsed != true) {
 				process[i].partitionID = partition[j].id;
 				process[i].status = "run";
 				partition[j].inUsed = true;
 				partition[j].processID = process[i].id;
+				waste[i] = partitionSize[j] - processSize[i];
 				partitionSize[j] -= processSize[i];
-				waste.push_back(partitionSize[j]);
 				break;
 			}
 		}
-		
 	}
 
 	cout << "===============================First Fit Algorithm==============================="<< endl;
 	cout << "Job ID\t\tPartition ID\t\tWaste\t\t\tStatus" << endl;
 	for (int i = 0; i < n; i++) {
-		cout << process[i].id << "\t\t" << process[i].partitionID << "\t\t\t" << waste[i] << "\t\t\t" << process[i].status << endl;
+		cout << process[i].id << "\t\t" << process[i].partitionID << "\t\t\t";
+
+		if (i < waste.size()) {
+			cout << waste.at(i) << "\t\t\t" << process[i].status << endl;
+		}
+		else {
+			cout << "0" << "\t\t\t" << process[i].status << endl;;
+		}
 	}
 	cout << "Total Waste: " << accumulate(waste.begin(), waste.end(), 0) << endl;
 	cout << "=================================================================================" << endl;
 }
 
 void nextFit(vector<int> partitionSize, int m, vector<int> processSize, int n, vector<Partition> partition, vector<Processes> process) {
+	vector<int> waste(m);
+	int j = 0;
 
+	for (int i = 0; i < n; i++) {
+		process[i].status = "wait";
+		while (j < m) {
+			if (partitionSize[j] >= processSize[i] && partition[j].inUsed != true) {
+				process[i].partitionID = partition[j].id;
+				process[i].status = "run";
+				partition[j].inUsed = true;
+				partition[j].processID = process[i].id;
+				waste[j] = partitionSize[j] - processSize[i];
+				partitionSize[j] -= processSize[i];
+				break;
+			}
+			j = (j + 1) % m;
+		}
+	}
+
+	cout << "================================Best Fit Algorithm================================" << endl;
+	cout << "Job ID\t\tPartition ID\t\tWaste\t\t\tStatus" << endl;
+	for (int i = 0; i < n; i++) {
+		cout << process[i].id << "\t\t" << process[i].partitionID << "\t\t\t" << waste.at(i) << "\t\t\t" << process[i].status << endl;
+	}
+	cout << "Total Waste: " << accumulate(waste.begin(), waste.end(), 0) << endl;
+	cout << "=================================================================================" << endl;
 }
 
 void bestFit(vector<int> partitionSize, int m, vector<int> processSize, int n, vector<Partition> partition, vector<Processes> process) {
